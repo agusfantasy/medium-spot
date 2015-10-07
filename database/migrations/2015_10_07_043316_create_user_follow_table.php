af75d88ac29c3d498616530085d5238485c54211
Agus <agusfantasy@gmail.com>
2015-10-07 12:07:46 +0700

    Rename class db migration create_user_follow and create_user_like

--- database/migrations/2015_10_07_043316_create_user_follow_table.php
+++ database/migrations/2015_10_07_043316_create_user_follow_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableUserFollow extends Migration
+class CreateUserFollowTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2015_10_07_044819_create_user_like_article_table.php
+++ database/migrations/2015_10_07_044819_create_user_like_article_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UserLikeArticleTable extends Migration
+class CreateUserLikeArticleTable extends Migration
 {
     /**
      * Run the migrations.
